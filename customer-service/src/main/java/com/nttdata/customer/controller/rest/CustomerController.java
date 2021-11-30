6408481f2a47761b7776fd56e6e610d2f11ff8e2
Juan Soto <jsotcast@nttdata.com>
2021-11-30 11:20:54 -0500

    REFACTOR ADD PROJECT

---
+++ customer-service/src/main/java/com/nttdata/customer/controller/rest/CustomerController.java
@@ -0,0 +1,35 @@
+package com.nttdata.customer.controller.rest;
+
+import com.nttdata.customer.entity.Customer;
+import com.nttdata.customer.service.ICustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final ICustomerService iCustomerService;
+
+    @GetMapping("/{id}")
+    public Mono<Customer> findById(@PathVariable String id){
+        return iCustomerService.findById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public Mono<Customer> findByName(@PathVariable String name){
+        return iCustomerService.findByName(name);
+    }
+
+    @GetMapping("/number/{number}")
+    public Mono<Customer> findByCustomerIdNumber(@PathVariable String number){
+        return iCustomerService.findByCustomerIdNumber(number);
+    }
+
+    @PostMapping
+    public Mono<Customer> save(@RequestBody Customer customer){
+        return iCustomerService.save(customer);
+    }
+}
---
+++ customer-service/src/main/java/com/nttdata/customer/entity/Customer.java
@@ -0,0 +1,27 @@
+package com.nttdata.customer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document("customer")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    private String id;
+    private String customerIdNumber;
+    private String customerType;
+    private String documentType;
+    private String documentNumber;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+}
